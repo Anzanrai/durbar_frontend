bc406120e967f68c97e0411e7d8ebd7e1d4afdd7
Anjan Rai <rai.unknown@gmail.com>
2022-09-28 16:31:03 +1000

    contact page completed

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './Contact.css';
 import Navbar from '../components/Navbar';
 
 function Contact() {
   return (
     <div className="pageWrapper">
       <Navbar />
+      <div className="contact-content-wrapper">
+        <div className="contact-content">
+          <h2>Remember us for a feast</h2>
+          <p>
+            To make an earliest booking for our services, please reach out to us
+            in our landline number 0228645212.
+          </p>
+          <p>
+            To get notified for any announcement and deals, or any new dish,
+            remember to follow us on our social media platforms.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
